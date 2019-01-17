97632637182f49f9ec0e3c58f743a90961716121
Kuzma <kuzmakulisin@macbook-pro-kuzma.local>
2019-01-17 12:37:28 +0300

    Added basic interface elements

---
+++ The best vacation for you WH 3.10/Controllers/WelcomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WelcomeViewController.swift
+//  The best vacation for you WH 3.10
+//
+//  Created by Kuzma on 16/01/2019.
+//  Copyright © 2019 Kuzma Kulishin. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
