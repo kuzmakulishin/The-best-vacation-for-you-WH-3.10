c19f5f5627b3a7750993b11d2ca484ee26d9c8a4
Kuzma <kuzmakulisin@macbook-pro-kuzma.local>
2019-01-19 13:18:48 +0300

    Created models and connections.

--- The best vacation for you WH 3.10/Controllers/QuestionViewController.swift
+++ The best vacation for you WH 3.10/Controllers/QuestionViewController.swift
@@ -9,11 +9,79 @@
 import UIKit
 
 class QuestionViewController: UIViewController {
+    
+    @IBOutlet weak var singleStackView: UIStackView!
+    
+    @IBOutlet weak var multipleStackView: UIStackView!
+    
+    @IBOutlet weak var rangedStackView: UIStackView!
+    
+    // вопрос, который выбран у нас
+    var questionIndex = 1
+    
+    var questions: [Question] = [
+        
+        Question(
+            text: "Какой отдых вы предпочитаете?",
+            type: .single,
+            answers: [
+           Answer(text: "Тусовка", type: .bar),
+           Answer(text: "Солнечный", type: .bitch),
+           Answer(text: "Активный", type: .sport),
+           Answer(text: "Домашний", type: .home),]
+        ),
+    
+        Question(
+            text: "Как вам нравится отдыхать?",
+            type: .multiple,
+            answers: [
+                Answer(text: "В компании друзей", type: .bar),
+                Answer(text: "Максимальный релакс", type: .bitch),
+                Answer(text: "Активный отдых", type: .sport),
+                Answer(text: "Побыть одному", type: .home),]
+        ),
+        
+        Question(
+            text: "на сколько вам нравится активный отдых?",
+            type: .ranged,
+            answers: [
+                Answer(text: "скорее да", type: .bar),
+                Answer(text: "скорее нет", type: .bitch),
+                Answer(text: "очень нравится", type: .sport),
+                Answer(text: "ненавижу", type: .home),]
+        )
+        
+        
+        
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        updateUI()
+        
+    }
+    
+    func updateUI() {
+        
+        singleStackView.isHidden = true
+        multipleStackView.isHidden = true
+        rangedStackView.isHidden = true
+        
+        navigationItem.title = "Вопрос № \(questionIndex + 1)"
+        
+        let question = questions[questionIndex]
+        
+        
+        switch question.type {
+        case .single:
+            singleStackView.isHidden = false
+        case .multiple:
+            multipleStackView.isHidden = false
+        case .ranged:
+            rangedStackView.isHidden = false
+        
+        }
     }
     
 
